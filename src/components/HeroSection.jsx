5a5f0abdf5d865f44541ef6e4591891aad309741
prabhat <prabhatshukla0849@gmail.com>
2024-11-21 19:59:20 +0530

    feat: completed herosection with video display functionality

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,6 @@
 import { ArrowBigLeft, ArrowRight } from "lucide-react"
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
 
 const HeroSection = () => {
   return <>
@@ -18,7 +20,7 @@ const HeroSection = () => {
       </p>
 
       <div className="flex justify-center mt-10 gap-x-6">
-      <a
+        <a
           href="#"
           className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
         >
@@ -26,9 +28,33 @@ const HeroSection = () => {
         </a>
         <a href="#" className="border flex gap-x-2 rounded-md px-3 py-2 bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent items-center">
           View Docs
-          <ArrowRight className="text-white"/>
-         
+          <ArrowRight className="text-white" />
+
         </a>
+      </div>
+
+      <div className="flex mt-10 justify-center">
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
+        >
+          <source src={video1} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
+        >
+          <source src={video2} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+
       </div>
 
     </div>
