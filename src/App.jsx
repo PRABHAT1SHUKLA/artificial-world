1090c31c2e94714130cd2feb5de420e17b51939c
prabhat <prabhatshukla0849@gmail.com>
2024-11-22 08:54:56 +0530

    feat: workflow section implementation initiated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './components/HeroSection'
 import FeatureSec from './components/Feature'
+import WorkFlow from './components/WorkFlow'
 
 
 
@@ -14,6 +15,7 @@ function App() {
         <div className='mx-auto px-6 pt-20 max-w-7xl'>
           <HeroSection/>
           <FeatureSec/>
+          <WorkFlow/>
 
         </div>
     </>
