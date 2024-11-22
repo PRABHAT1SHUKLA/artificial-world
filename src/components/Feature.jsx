6a64517913276609ac7759b680b0b54c285f8851
prabhat <prabhatshukla0849@gmail.com>
2024-11-22 08:39:27 +0530

    feat: feature card implemented

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -17,18 +17,25 @@ const FeatureSec = () => {
         </h2>
 
       </div>
-    
-     <div className="flex ">
-      {features.map((feature , index)=>(
-        <div key={index}>
-          <div className="flex">
-          <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
+
+      <div className="flex mt-10 lg:mt-20 flex-wrap ">
+        {features.map((feature, index) => (
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
+            <div className="flex">
+              <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
                 {feature.icon}
+
               </div>
+              <div>
+                <h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
+                <p className="text-md p-2 mb-20 text-neutral-500">
+                  {feature.description}
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
-     </div>
+        ))}
+      </div>
     </div>
   )
 
